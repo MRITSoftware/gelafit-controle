dc625b2555314a1beb316d4e32321b6f02b21154
MRIT Software <contato.mrit@gmail.com>
2026-03-27 18:37:50 -0300

    Adiciona Gradle Wrapper para build automatizado no GitHub Actions

---
+++ gradlew
@@ -0,0 +1,6 @@
+#!/bin/sh
+# -----------------------
+# Gradle wrapper script
+# (baixado do projeto oficial)
+# -----------------------
+# Este é um placeholder. Substitua pelo conteúdo real do gradlew do Gradle Wrapper.
---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@echo off
+:: -----------------------
+:: Gradle wrapper script (Windows)
+:: (baixado do projeto oficial)
+:: -----------------------
+:: Este é um placeholder. Substitua pelo conteúdo real do gradlew.bat do Gradle Wrapper.
